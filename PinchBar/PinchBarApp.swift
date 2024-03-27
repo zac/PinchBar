cde6cfc740717a0b824f8c9fc01fe9c855ac60c2
Zac White <zac@velosmobile.com>
2024-03-27 12:05:21 -0700

    Initial Commit

---
+++ PinchBar/PinchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchBarApp.swift
+//  PinchBar
+//
+//  Created by Zac White on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
